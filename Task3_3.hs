ed620799c97328e8cfef44c42f39b518635a0afd
Anastasiia Temnova <temnova@kspt.icc.spbstu.ru>
2018-10-27 01:22:42 +0300

    task3.3

--- Task3_3.hs
+++ Task3_3.hs
@@ -14,38 +14,43 @@ instance Semigroup (PSet a) where
 instance Monoid (PSet a) where
     mempty = PSet{ contains = f } where f _ = False
     mappend PSet{ contains = contains_a } PSet{ contains = contains_b } = PSet{ contains = f }
-        where f a | contains_a a == True = True
-                  | contains_b a == True = True
-                  | otherwise = False
+        where f a = (contains_a a) || (contains_b a)
+
+newtype PSet2 a = PSet2{ contains2 :: (a -> Bool) }
+
+instance Semigroup (PSet2 a) where
+    (<>) a b = mappend a b
 
 -- Пересечение множеств
 -- Нейтральное значение - универсум (чтобы выполнялся осн.закон моноида)
 -- Функция добавления - объект входит и в одно множество, и во второе
-instance Monoid (PSet a) where
-    mempty = PSet{ contains = f } where f _ = True
-    mappend PSet{ contains = contains_a } PSet{ contains = contains_b } = PSet{ contains = f }
-        where f a | (contains_a a == True) && (contains_b a == True) = True
-                  | otherwise = False
+instance Monoid (PSet2 a) where
+    mempty = PSet2{ contains2 = f } where f _ = True
+    mappend PSet2{ contains2 = contains_a } PSet2{ contains2 = contains_b } = PSet2{ contains2 = f }
+        where f a = (contains_a a) && (contains_b a)
 
 -- Разность реализовать не получилось, так как не ясно как определять нейтральное значение
 -- Аналогично с декартовым произведением
 
+newtype PSet3 a = PSet3{ contains3 :: (a -> Bool) }
+
+instance Semigroup (PSet3 a) where
+    (<>) a b = mappend a b
+
 -- Вспомогательные функции для следующей монады
-razn PSet{ contains = contains_a } PSet{ contains = contains_b } = PSet{ contains = f }
-    where f a | contains_b a == True = False
-              | contains_a a == True = True
+razn PSet3{ contains3 = contains_a } PSet3{ contains3 = contains_b } = PSet3{ contains3 = f }
+    where f a | contains_b a = False
+              | contains_a a = True
               | otherwise = False
 
-objed PSet{ contains = contains_a } PSet{ contains = contains_b } = PSet{ contains = f }
-    where f a | contains_a a == True = True
-              | contains_b a == True = True
-              | otherwise = False
+objed PSet3{ contains3 = contains_a } PSet3{ contains3 = contains_b } = PSet3{ contains3 = f }
+    where f a = (contains_a a) || (contains_b a)
 
 -- Симметричная разность
 -- Нейтральное - пустое
 -- Берем разности и объединяем :)
-instance Monoid (PSet a) where
-    mempty = PSet{ contains = f } where f _ = False
+instance Monoid (PSet3 a) where
+    mempty = PSet3{ contains3 = f } where f _ = False
     mappend b c = objed (razn b c) (razn c b)
 
 -- С функторами тоже не сложилось. Ведь у нас лежат в мн-ве не значения,
@@ -53,14 +58,14 @@ instance Monoid (PSet a) where
 -- принадлежит ли объект мн-ву или нет.
 -- И что к кому куда применять?
 
--- Например, наша функция такая
+-- Например, наша функция такая:
 -- f 1 = True
 -- f 2 = True
 -- f 3 = True
 -- f _ = False
 
 -- А мы хотим ко всем эл-там применить функцию квадрат.
--- Тогда логично, что теперь должно быть мн-во со след.функцией:
+-- Тогда логично, что теперь должно быть мн-во со след. функцией:
 -- f 1 = True
 -- f 4 = True
 -- f 9 = True
@@ -72,5 +77,4 @@ instance Monoid (PSet a) where
 
 -- instance Functor (PSet) where
 --     fmap f PSet{ contains = fa } = PSet{ contains = fb }
---         where fb (f a) | fa a == True = True
---                        | otherwise = False
+--         where fb (f a) = fa a 
