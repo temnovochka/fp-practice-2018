8ef8ae2f20f6554ed150a40f2fcda757b46812b6
Anastasiia Temnova <temnova@kspt.icc.spbstu.ru>
2018-10-31 17:17:30 +0300

    task 4

--- Task4_1.hs
+++ Task4_1.hs
@@ -5,3 +5,15 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor (FunMonad) where
+    fmap f FunMonad { fun = myF } = FunMonad { fun = newF } where newF = f.myF
+
+instance Applicative (FunMonad) where
+    pure = return
+    f <*> x = f >>= \f' -> x >>= \x' -> return (f' x')
+
+instance Monad (FunMonad) where
+    FunMonad { fun = myF } >>= f = FunMonad { fun = newF } 
+        where newF str = fun (f (myF str)) str
+    return a = FunMonad { fun = f } where f str = a
